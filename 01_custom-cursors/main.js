83e9c32d0007cdf70ec24469a4bff1ad1d7a6541
Neef Rehman <neef.rehman@gmail.com>
2020-10-22 19:18:44 +0100

    update cursor

--- 01_custom-cursors/main.js
+++ 01_custom-cursors/main.js
@@ -4,3 +4,20 @@ const forwardButton = document.querySelector(".control.forward");
 
 forwardButton.addEventListener("click", () => carousel.scrollBy({ left: 1 }));
 backwardButton.addEventListener("click", () => carousel.scrollBy({ left: -1 }));
+
+// JS cursor
+if (matchMedia("(pointer:fine)").matches) {
+    const cursor = document.querySelector(".cursor");
+
+    const allLinks = document.querySelectorAll("a");
+
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+        // cursor.classList.add("show");
+    });
+
+    allLinks.forEach((link) => {
+        link.addEventListener("mouseenter", () => cursor.classList.add("large"));
+        link.addEventListener("mouseleave", () => cursor.classList.remove("large"));
+    });
+}
