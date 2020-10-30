dde92573be102a73a5841b9bbfbf5ddbd89630de
Neef Rehman <neef.rehman@gmail.com>
2020-10-30 16:17:35 +0000

    add comments and consolidate cursor styles

--- 01_custom-cursors/main.js
+++ 01_custom-cursors/main.js
@@ -2,6 +2,8 @@ const carousel = document.querySelector(".carousel");
 const backwardButton = document.querySelector(".control.backward");
 const forwardButton = document.querySelector(".control.forward");
 
+
+// Image width + image border
 const imageWidth = carousel.querySelector("img").clientWidth + 5;
 
 forwardButton.addEventListener("click", () =>
@@ -11,19 +13,25 @@ backwardButton.addEventListener("click", () =>
     carousel.scrollBy({ left: imageWidth * -1 })
 );
 
+
 // JS cursor
 if (matchMedia("(pointer:fine)").matches) {
     const cursor = document.querySelector(".cursor");
-
     const allLinks = document.querySelectorAll("a");
 
-    const getMean = (array) =>
-        array.reduce((acc, cur) => acc + cur, 0) / array.length;
-    let previousMouseXArray = [0, 0, 0, 0, 0];
+    // Use an array of the mouse's previous X position so that we can get an average
+    let previousMouseXArray = Array(30).fill(0);
+
+    // Get the average from our array of positions
+    const getAverageMouseX = () =>
+        previousMouseXArray.reduce((acc, cur) => acc + cur) / array.length;
 
     document.addEventListener("mousemove", (e) => {
         cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-        if (e.clientX > getMean(previousMouseXArray)) {
+
+        // Using an average helps make the transition smoother, as the fish
+        // wont change state if the mouse only moves a few pixels
+        if (e.clientX > getAverageMouseX()) {
             cursor.classList.remove("left");
             cursor.classList.add("right");
         } else {
@@ -31,10 +39,13 @@ if (matchMedia("(pointer:fine)").matches) {
             cursor.classList.add("left");
         }
 
+        // Here we remove the first item in out array
         previousMouseXArray.shift();
-        previousMouseXArray = [...previousMouseXArray, e.clientX];
+        // and then add our current position to the end of the array for next time this event fires
+        previousMouseXArray = previousMouseXArray.concat(e.clientX);
     });
 
+    // Here we add the various classes to style our cursor depending on where the mouse is
     allLinks.forEach((link) => {
         link.addEventListener("mouseenter", () => cursor.classList.add("up"));
         link.addEventListener("mouseleave", () => cursor.classList.remove("up"));
