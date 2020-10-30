6a2fc4688e9b860b22f180ec6a3d6236d6638bba
Neef Rehman <neef.rehman@gmail.com>
2020-10-30 15:10:12 +0000

    nicer fish interactions :)

--- 01_custom-cursors/main.js
+++ 01_custom-cursors/main.js
@@ -17,8 +17,22 @@ if (matchMedia("(pointer:fine)").matches) {
 
     const allLinks = document.querySelectorAll("a");
 
+    const getMean = (array) =>
+        array.reduce((acc, cur) => acc + cur, 0) / array.length;
+    let previousMouseXArray = [0, 0, 0, 0, 0];
+
     document.addEventListener("mousemove", (e) => {
         cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+        if (e.clientX > getMean(previousMouseXArray)) {
+            cursor.classList.remove("left");
+            cursor.classList.add("right");
+        } else {
+            cursor.classList.remove("right");
+            cursor.classList.add("left");
+        }
+
+        previousMouseXArray.shift();
+        previousMouseXArray = [...previousMouseXArray, e.clientX];
     });
 
     allLinks.forEach((link) => {
@@ -27,16 +41,16 @@ if (matchMedia("(pointer:fine)").matches) {
     });
 
     forwardButton.addEventListener("mouseenter", () =>
-        cursor.classList.add("right")
+        cursor.classList.add("forwards")
     );
     forwardButton.addEventListener("mouseleave", () =>
-        cursor.classList.remove("right")
+        cursor.classList.remove("forwards")
     );
 
     backwardButton.addEventListener("mouseenter", () =>
-        cursor.classList.add("left")
+        cursor.classList.add("backwards")
     );
     backwardButton.addEventListener("mouseleave", () =>
-        cursor.classList.remove("left")
+        cursor.classList.remove("backwards")
     );
 }
